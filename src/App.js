57841d54e910952721f29d473bbaf5ba00698a05
Mritunjay Agarwal <mritunjayagarwal96@gmail.com>
2024-01-31 00:55:09 +0530

    Added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Speed from './images/Speed.png';
 import OwlCarousel from 'react-owl-carousel';
 import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'owl.carousel/dist/assets/owl.theme.default.css';
+import { Fade } from "react-awesome-reveal";
 
 function App() {
   const responsive = {
@@ -39,11 +40,13 @@ function App() {
           <img src={require('./images/logo.png')} className='hero-prop' alt="" />
           <div className='container'>
             <div className='row'>
-            <div className='order-md-2 col-lg-6 d-flex align-items-center justify-content-center'>
+              <div className='order-md-2 col-lg-6 d-flex align-items-center justify-content-center'>
                 <div className='tokenomic-card' style={{ width: "80%" }}>
-                  <img src={require('./images/about/2.png')} className='img-fluid' style={{ width: "60%" }} alt="" />
-                  <h1 className='text-white mt-4' style={{ fontSize: "80px" }}>AePOS</h1>
-                  <p className='tokenomic-para'>Network</p>
+                  <Fade cascade>
+                    <img src={require('./images/about/2.png')} className='img-fluid' style={{ width: "60%" }} alt="" />
+                    <h1 className='text-white mt-4' style={{ fontSize: "80px" }}>AePOS</h1>
+                    <p className='tokenomic-para'>Network</p>
+                  </Fade>
                 </div>
               </div>
               <div className='order-md-1 col-lg-6'>
@@ -66,11 +69,13 @@ function App() {
               {
                 tokenomics.map((tokenomic) => {
                   return <div className='col-lg-3'>
-                    <div className='tokenomic-card'>
-                      <img src={require(`../public/tokenomics/${tokenomic.image}`)} className='img-fluid tokenomic-img' alt="" />
-                      <h1 className='tokenomic-head'>{tokenomic.title}</h1>
-                      <p className='tokenomic-para'>{tokenomic.description}</p>
-                    </div>
+                    <Fade className='h-100' direction='up' key={tokenomic.id} delay={parseInt(tokenomic.id * '200')}>
+                      <div className='tokenomic-card'>
+                        <img src={require(`../public/tokenomics/${tokenomic.image}`)} className='img-fluid tokenomic-img' alt="" />
+                        <h1 className='tokenomic-head'>{tokenomic.title}</h1>
+                        <p className='tokenomic-para'>{tokenomic.description}</p>
+                      </div>
+                    </Fade>
                   </div>
                 })
               }
@@ -83,14 +88,18 @@ function App() {
               <div className='row'>
                 <div className='col-lg-6'>
                   <div className="speed-wrapper">
-                    <img src={Speed} className='img-fluid' alt="" />
+                    <Fade>
+                      <img src={Speed} className='img-fluid' alt="" />
+                    </Fade>
                   </div>
                 </div>
                 <div className='col-lg-6'>
-                  <h1 className='about-head'>The Fastest Blockchain</h1>
-                  <p className='about-para'>The Tectum Blockchain exceeds 1 million transactions per second using a proprietary Proof-of-Utility Consensus.</p>
-                  <p className='about-para'>This makes Tectum the perfect solution as an “Overlay Network” for numerous blockchain systems such as Bitcoin.</p>
-                  <p className='about-para'>Learn about Tectum Blockchain</p>
+                  <Fade cascade>
+                    <h1 className='about-head'>The Fastest Blockchain</h1>
+                    <p className='about-para'>The Tectum Blockchain exceeds 1 million transactions per second using a proprietary Proof-of-Utility Consensus.</p>
+                    <p className='about-para'>This makes Tectum the perfect solution as an “Overlay Network” for numerous blockchain systems such as Bitcoin.</p>
+                    <p className='about-para'>Learn about Tectum Blockchain</p>
+                  </Fade>
                 </div>
               </div>
             </div>
@@ -99,14 +108,18 @@ function App() {
         <section className='fastest-blockchain-section'>
           <div className='black-wrapper'>
             <div className='container'>
-              <h1 className='text-center fastest-blockchain-head'>The Fastest Blockchain for All Industries</h1>
-              <p className='text-center fastest-blockchain-para'>Tectum is the world’s fastest blockchain – a lightning-fast distributed ledger powering the superconductive digital financial environment of the future.</p>
+              <Fade cascade>
+                <h1 className='text-center fastest-blockchain-head'>The Fastest Blockchain for All Industries</h1>
+                <p className='text-center fastest-blockchain-para'>Tectum is the world’s fastest blockchain – a lightning-fast distributed ledger powering the superconductive digital financial environment of the future.</p>
+              </Fade>
               <div className='row mt-5'>
                 <div className='col-lg-4'>
                   <div className='tokenomic-card d-flex align-items-center justify-content-center'>
                     <div>
-                      <img src={require('./images/about/1.png')} className='img-fluid' alt="" />
-                      <p className='fastest-blockchain-card-para'>Exceeds 1 million TPS and provides instant event status delivery & ownership updates.</p>
+                      <Fade cascade>
+                        <img src={require('./images/about/1.png')} className='img-fluid' alt="" />
+                        <p className='fastest-blockchain-card-para'>Exceeds 1 million TPS and provides instant event status delivery & ownership updates.</p>
+                      </Fade>
                     </div>
                   </div>
                 </div>
@@ -116,8 +129,10 @@ function App() {
                 <div className='col-lg-4'>
                   <div className='tokenomic-card d-flex align-items-center justify-content-center'>
                     <div>
-                      <img src={require('./images/about/1.png')} className='img-fluid' alt="" />
-                      <p className='fastest-blockchain-card-para'>Exceeds 1 million TPS and provides instant event status delivery & ownership updates.</p>
+                      <Fade cascade>
+                        <img src={require('./images/about/1.png')} className='img-fluid' alt="" />
+                        <p className='fastest-blockchain-card-para'>Exceeds 1 million TPS and provides instant event status delivery & ownership updates.</p>
+                      </Fade>
                     </div>
                   </div>
                 </div>
@@ -129,17 +144,21 @@ function App() {
           <div className='container'>
             <div className='row'>
               <div className='col-lg-6'>
-                <h1 className='solutions-head'>Crypto Solutions for Business</h1>
-                <p className='solutions-para'>
-                  The SoftNote is the first ever ‘transactionless’ product with instant payment zero-fee processing for an end-user. The SoftNote has ‘physical cash’ elements, for example, a Bitcoin SoftNote bill can be printed on paper, does not require a confirmation from the Bitcoin network, incurs no fees, and has no geographical boundaries.
-                </p>
-                <p className='solutions-para'>
-                  Therefore, the SoftNote can be used person-to- person, sent over any messenger application as a picture file or sent by wallet to wallet. A SoftNote is secured by a passcode rendering it useless to an unauthorised possessor.
-                </p>
-                <p className='solutions-para'>Learn about SoftNote</p>
+                <Fade cascade>
+                  <h1 className='solutions-head'>Crypto Solutions for Business</h1>
+                  <p className='solutions-para'>
+                    The SoftNote is the first ever ‘transactionless’ product with instant payment zero-fee processing for an end-user. The SoftNote has ‘physical cash’ elements, for example, a Bitcoin SoftNote bill can be printed on paper, does not require a confirmation from the Bitcoin network, incurs no fees, and has no geographical boundaries.
+                  </p>
+                  <p className='solutions-para'>
+                    Therefore, the SoftNote can be used person-to- person, sent over any messenger application as a picture file or sent by wallet to wallet. A SoftNote is secured by a passcode rendering it useless to an unauthorised possessor.
+                  </p>
+                  <p className='solutions-para'>Learn about SoftNote</p>
+                </Fade>
               </div>
               <div className='col-lg-6 d-flex align-items-center justify-content-center'>
-                <img src={require('./images/solutions.png')} className='img-fluid bnw' alt="" />
+                <Fade direction='right' delay={1000}>
+                  <img src={require('./images/solutions.png')} className='img-fluid bnw' alt="" />
+                </Fade>
               </div>
             </div>
           </div>
@@ -148,17 +167,21 @@ function App() {
           <div className='container'>
             <div className='row'>
               <div className='col-lg-6 d-flex align-items-center justify-content-center'>
+                <Fade delay={1000} direction='left'>
                 <img src={require('./images/authentication.png')} className='img-fluid bnw' alt="" />
+                </Fade>
               </div>
               <div className='col-lg-6'>
-                <h1 className='authentication-head'>Crypto Solutions for Business</h1>
-                <p className='authentication-para'>
-                  The SoftNote is the first ever ‘transactionless’ product with instant payment zero-fee processing for an end-user. The SoftNote has ‘physical cash’ elements, for example, a Bitcoin SoftNote bill can be printed on paper, does not require a confirmation from the Bitcoin network, incurs no fees, and has no geographical boundaries.
-                </p>
-                <p className='authentication-para'>
-                  Therefore, the SoftNote can be used person-to- person, sent over any messenger application as a picture file or sent by wallet to wallet. A SoftNote is secured by a passcode rendering it useless to an unauthorised possessor.
-                </p>
-                <p>Learn about SoftNote</p>
+                <Fade cascade>
+                  <h1 className='authentication-head'>Crypto Solutions for Business</h1>
+                  <p className='authentication-para'>
+                    The SoftNote is the first ever ‘transactionless’ product with instant payment zero-fee processing for an end-user. The SoftNote has ‘physical cash’ elements, for example, a Bitcoin SoftNote bill can be printed on paper, does not require a confirmation from the Bitcoin network, incurs no fees, and has no geographical boundaries.
+                  </p>
+                  <p className='authentication-para'>
+                    Therefore, the SoftNote can be used person-to- person, sent over any messenger application as a picture file or sent by wallet to wallet. A SoftNote is secured by a passcode rendering it useless to an unauthorised possessor.
+                  </p>
+                  <p>Learn about SoftNote</p>
+                </Fade>
               </div>
             </div>
           </div>
@@ -167,6 +190,7 @@ function App() {
           <div className='container'>
             <div className='row'>
               <div className='col-lg-6'>
+                <Fade cascade>
                 <h1 className='tia-ai-head'>Crypto Solutions for Business</h1>
                 <p className='tia-ai-para'>
                   The SoftNote is the first ever ‘transactionless’ product with instant payment zero-fee processing for an end-user. The SoftNote has ‘physical cash’ elements, for example, a Bitcoin SoftNote bill can be printed on paper, does not require a confirmation from the Bitcoin network, incurs no fees, and has no geographical boundaries.
@@ -175,9 +199,12 @@ function App() {
                   Therefore, the SoftNote can be used person-to- person, sent over any messenger application as a picture file or sent by wallet to wallet. A SoftNote is secured by a passcode rendering it useless to an unauthorised possessor.
                 </p>
                 <p>Learn about SoftNote</p>
+                </Fade>
               </div>
               <div className='col-lg-6 d-flex align-items-center justify-content-center'>
+                <Fade delay={1000} direction='right'>
                 <img src={require('./images/Tia-ai.png')} className='img-fluid bnw' alt="" />
+                </Fade>
               </div>
             </div>
           </div>
@@ -216,7 +243,7 @@ function App() {
             </div>
           </OwlCarousel>
         </section>
-        <section>
+        <section style={{paddingTop: 0}}>
           <div className='container mb-5'>
             <h1 className='partners-head' style={{ color: "#000" }}>Our Partners</h1>
           </div>
@@ -245,10 +272,12 @@ function App() {
           <div className='container'>
             <div className='row'>
               <div className='col-lg-6'>
+                <Fade cascade>
                 <h1 className='final-head-1'>Let’s go</h1>
                 <h1 className='final-head-2 mt-4'>Tectum Testnet</h1>
                 <p className='final-para mt-4'>Start with our Test Node</p>
                 <button className='hero-btn mt-4'>Start with our Test Node</button>
+                </Fade>
               </div>
             </div>
           </div>
