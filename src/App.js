f144e5a7624d4daa4862c68b75ae9b92fbc6ac48
Mritunjay Agarwal <mritunjayagarwal96@gmail.com>
2024-02-01 22:28:24 +0530

    Added

--- src/App.js
+++ src/App.js
@@ -28,15 +28,59 @@ function App() {
   return (
     <>
       <main>
-        <section className='top-bar'>
+        {/* <section className='top-bar'>
           <div className='container'>
             <div className='d-flex align-items-center justify-content-center pt-3'>
-              <p class="px-2"><img src={require('./images/T.png')} className='img-fluid' style={{ marginTop: "-5px" }} alt="" />Get Your $TET Tokens</p>
-              <p class="px-2">/</p>
-              <p class="px-2"><img src={require('./images/T.png')} className='img-fluid' style={{ marginTop: "-5px" }} alt="" />Stake $TET</p>
+              <p className="px-2"><img src={require('./images/T.png')} className='img-fluid' style={{ marginTop: "-5px" }} alt="" />Get Your $TET Tokens</p>
+              <p className="px-2">/</p>
+              <p className="px-2"><img src={require('./images/T.png')} className='img-fluid' style={{ marginTop: "-5px" }} alt="" />Stake $TET</p>
             </div>
           </div>
-        </section>
+        </section> */}
+        <nav class="navbar navbar-expand-lg py-1 m-0 top-bar" data-aos="fade-down" data-aos-delay="2300"
+            data-aos-duration="500">
+            <div class="container">
+                <a class="navbar-brand" href="/">
+                    <img src={require('./images/tectum.png')} class="img-fluid" alt="" /></a>
+                <button class="navbar-toggler text-white" type="button" data-toggle="collapse" data-target="#navbarText"
+                    aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                    <i class="fa fa-bars"></i>
+                </button>
+                <div class="collapse navbar-collapse text-white" id="navbarText">
+                    <ul class="navbar-nav me-auto mx-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a href="/" class="px-3 pt-2 btn-link">
+                                Home
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class='px-3 pt-2 btn-link' href='/xnetwork'>
+                                Network
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class='px-3 pt-2 btn-link' href='/nft'>
+                                NFT’s
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class='px-3 pt-2 btn-link' href='/nft'>
+                                Contact Us
+                            </a>
+                        </li>
+                    </ul>
+                    <ul class="navbar-nav mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a href='/contact' style={{color: '#fff'}}>
+                                <button type="button" class="px-5 btn pt-2 hero-btn" style={{fontWeight: 400}}>
+                                    Buy Now
+                                </button>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
         <section className='hero-section'>
           <div id='stars'></div>
           <div id='stars2'></div>
@@ -227,22 +271,22 @@ function App() {
             <h1 className='partners-head' style={{ color: "#000" }}>Our Partners</h1>
           </div>
           <OwlCarousel className='owl-theme' loop items={6} nav={false} dots={false} responsive={responsive} autoplay={true} autoplayTimeout={3000}>
-            <div class='item d-flex align-items-center'>
+            <div className='item d-flex align-items-center'>
               <img src={require('./images/partners/1.png')} className='img-fluid' style={{ width: "180px" }} alt="" />
             </div>
-            <div class='item d-flex align-items-center'>
+            <div className='item d-flex align-items-center'>
               <img src={require('./images/partners/2.png')} className='img-fluid' style={{ width: "180px" }} alt="" />
             </div>
-            <div class='item d-flex align-items-center'>
+            <div className='item d-flex align-items-center'>
               <img src={require('./images/partners/3.png')} className='img-fluid' style={{ width: "180px" }} alt="" />
             </div>
-            <div class='item d-flex align-items-center'>
+            <div className='item d-flex align-items-center'>
               <img src={require('./images/partners/4.png')} className='img-fluid' style={{ width: "180px" }} alt="" />
             </div>
-            <div class='item d-flex align-items-center'>
+            <div className='item d-flex align-items-center'>
               <img src={require('./images/partners/5.png')} className='img-fluid' style={{ width: "180px" }} alt="" />
             </div>
-            <div class='item d-flex align-items-center'>
+            <div className='item d-flex align-items-center'>
               <img src={require('./images/partners/6.png')} className='img-fluid' style={{ width: "180px" }} alt="" />
             </div>
           </OwlCarousel>
@@ -252,22 +296,22 @@ function App() {
             <h1 className='partners-head' style={{ color: "#000" }}>Our Partners</h1>
           </div>
           <OwlCarousel className='owl-theme' loop items={6} nav={false} dots={false} responsive={responsive} autoplay={true} autoplayTimeout={5000}>
-            <div class='item d-flex align-items-center'>
+            <div className='item d-flex align-items-center'>
               <img src={require('./images/partners/1.png')} className='img-fluid' style={{ width: "180px" }} alt="" />
             </div>
-            <div class='item d-flex align-items-center'>
+            <div className='item d-flex align-items-center'>
               <img src={require('./images/partners/2.png')} className='img-fluid' style={{ width: "180px" }} alt="" />
             </div>
-            <div class='item d-flex align-items-center'>
+            <div className='item d-flex align-items-center'>
               <img src={require('./images/partners/3.png')} className='img-fluid' style={{ width: "180px" }} alt="" />
             </div>
-            <div class='item d-flex align-items-center'>
+            <div className='item d-flex align-items-center'>
               <img src={require('./images/partners/4.png')} className='img-fluid' style={{ width: "180px" }} alt="" />
             </div>
-            <div class='item d-flex align-items-center'>
+            <div className='item d-flex align-items-center'>
               <img src={require('./images/partners/5.png')} className='img-fluid' style={{ width: "180px" }} alt="" />
             </div>
-            <div class='item d-flex align-items-center'>
+            <div className='item d-flex align-items-center'>
               <img src={require('./images/partners/6.png')} className='img-fluid' style={{ width: "180px" }} alt="" />
             </div>
           </OwlCarousel>
@@ -286,34 +330,115 @@ function App() {
             </div>
           </div>
         </section>
-        <footer>
-          <div className='container'>
-            <div className='row'>
-              <div className='col-lg-6'>
-                <img src={require('./images/tectum.png')} className='img-fluid' style={{ marginTop: "-10px" }} alt="" />
-              </div>
-              <div className='col-lg-3'>
-                <div>
-                  <div className='d-flex justify-content-center'>
-                    <div className='mr-4'>
-                      <p className='footer-item'>Product</p>
+        <footer className="footer-section">
+          <div className="container">
+            <div className="footer-cta pt-5 pb-5">
+              <div className="row">
+                <div className="col-xl-4 col-md-4 mb-30">
+                  <div className="single-cta">
+                    <i className="fas fa-map-marker-alt"></i>
+                    <div className="cta-text">
+                      <h4>Find us</h4>
+                      <span>1010 Avenue, sw 54321, chandigarh</span>
                     </div>
-                    <div className='mr-4'>
-                      <p className='footer-item'>About</p>
+                  </div>
+                </div>
+                <div className="col-xl-4 col-md-4 mb-30">
+                  <div className="single-cta">
+                    <i className="fas fa-phone"></i>
+                    <div className="cta-text">
+                      <h4>Call us</h4>
+                      <span>9876543210 0</span>
                     </div>
-                    <div className='mr-4'>
-                      <p className='footer-item'>FAQ</p>
-                    </div>
-                    <div className='mr-4'>
-                      <p className='footer-item'>Blog</p>
+                  </div>
+                </div>
+                <div className="col-xl-4 col-md-4 mb-30">
+                  <div className="single-cta">
+                    <i className="far fa-envelope-open"></i>
+                    <div className="cta-text">
+                      <h4>Mail us</h4>
+                      <span>mail@info.com</span>
                     </div>
                   </div>
                 </div>
               </div>
-              <div className='col-lg-3'>
-                <button type="button" class="btn uni-btn btn-block">
-                  Join Presale
-                </button>
+            </div>
+            <div className="footer-content pt-5 pb-5">
+              <div className="row">
+                <div className="col-xl-4 col-lg-4 mb-50">
+                  <div className="footer-widget">
+                    <div className="footer-logo">
+                      <a href="index.html"><img src={require('./images/tectum.png')} className="img-fluid" alt="logo" /></a>
+                    </div>
+                    <div className="footer-text">
+                      <p>Lorem ipsum dolor sit amet, consec tetur adipisicing elit, sed do eiusmod tempor incididuntut consec tetur adipisicing
+                        elit,Lorem ipsum dolor sit amet.</p>
+                    </div>
+                    <div className="footer-social-icon">
+                      <span>Follow us</span>
+                      <a href="#"><i className="fab fa-facebook-f facebook-bg"></i></a>
+                      <a href="#"><i className="fab fa-twitter twitter-bg"></i></a>
+                      <a href="#"><i className="fab fa-google-plus-g google-bg"></i></a>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-xl-4 col-lg-4 col-md-6 mb-30">
+                  <div className="footer-widget">
+                    <div className="footer-widget-heading">
+                      <h3>Useful Links</h3>
+                    </div>
+                    <ul>
+                      <li><a href="#">Home</a></li>
+                      <li><a href="#">about</a></li>
+                      <li><a href="#">services</a></li>
+                      <li><a href="#">portfolio</a></li>
+                      <li><a href="#">Contact</a></li>
+                      <li><a href="#">About us</a></li>
+                      <li><a href="#">Our Services</a></li>
+                      <li><a href="#">Expert Team</a></li>
+                      <li><a href="#">Contact us</a></li>
+                      <li><a href="#">Latest News</a></li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="col-xl-4 col-lg-4 col-md-6 mb-50">
+                  <div className="footer-widget">
+                    <div className="footer-widget-heading">
+                      <h3>Subscribe</h3>
+                    </div>
+                    <div className="footer-text mb-25">
+                      <p>Don’t miss to subscribe to our new feeds, kindly fill the form below.</p>
+                    </div>
+                    <div className="subscribe-form">
+                      <form action="#">
+                        <input type="text" placeholder="Email Address" />
+                          <button><i className="fab fa-telegram-plane"></i></button>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="copyright-area">
+            <div className="container">
+              <div className="row">
+                <div className="col-xl-6 col-lg-6 text-center text-lg-left">
+                  <div className="copyright-text">
+                    <p>Copyright &copy; 2018, All Rights Reserved</p>
+                  </div>
+                </div>
+                <div className="col-xl-6 col-lg-6 d-none d-lg-block text-right">
+                  <div className="footer-menu">
+                    <ul>
+                      <li><a href="#">Home</a></li>
+                      <li><a href="#">Terms</a></li>
+                      <li><a href="#">Privacy</a></li>
+                      <li><a href="#">Policy</a></li>
+                      <li><a href="#">Contact</a></li>
+                    </ul>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
